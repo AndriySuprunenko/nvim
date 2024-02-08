55a7300be0f50694fc94fc58d218d7123d0e3351
AndriySuprunenko <43989070+AndriySuprunenko@users.noreply.github.com>
2024-02-08 18:19:24 +0200

    delete copilot

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,8 +1,11 @@
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set number")
+-- vim.cmd("set relativenumber")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set smartindent")
+vim.cmd("set autoindent")
 vim.g.mapleader = " "
 -- Explorer
 vim.keymap.set("n", "<leader>x", ":Ex<CR>", {})
